d64db24918ec0f7c1afddad223063f14ddb8a529
Anonymouskeys <admin@anonymouskeys.com>
2026-07-25 20:51:54 +0500

    Give Dragon VPN a separate application ID

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.anonymouskeys.monstervpn"
+        applicationId = "com.anonymouskeys.dragonvpn"
         minSdk = 24
         targetSdk = 36
         versionCode = 738
